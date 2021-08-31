f1285248fe9840e6ca40d04ef57f8803fcb8653a
Tim Kräuter <tim.kraeuterr@gmail.com>
2021-08-31 11:45:06 +0200

    Adding github pages sources and build task + updated dependencies in debugger

--- modeler/starter/webpack.config.js
+++ modeler/starter/webpack.config.js
@@ -4,54 +4,63 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const SOURCE_VERSION = process.env.SOURCE_VERSION || process.env.npm_package_gitHead || 'dev';
 
-module.exports = {
-  entry: {
-    bundle: ['./app/app.js'],
-  },
-  output: {
-    path: __dirname + '/public',
-    filename: 'app.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.xml$/,
-        use: 'raw-loader',
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.less$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
+module.exports = (env) => {
+
+  let outputPath = __dirname + '/public';
+  if (env.production) {
+
+    // We expect the Visual debugger project in the same folder as this project.
+    outputPath = __dirname + '/../../github-pages';
+  }
+  return {
+    entry: {
+      bundle: ['./app/app.js'],
+    },
+    output: {
+      path: outputPath,
+      filename: 'app.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.xml$/,
+          use: 'raw-loader',
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: ['file-loader'],
+        },
+        {
+          test: /\.less$/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
             },
-          },
-          {
-            loader: 'less-loader',
-            options: {
-              sourceMap: true,
+            {
+              loader: 'less-loader',
+              options: {
+                sourceMap: true,
+              },
             },
-          },
-        ],
-      }
+          ],
+        }
+      ],
+    },
+    plugins: [
+      new CopyWebpackPlugin({ patterns: [{ from: '**/*.{html,css,woff,ttf,eot,svg,woff2,ico}', context: 'app/' }] }),
+      new webpack.DefinePlugin({
+        'process.env.SOURCE_VERSION': JSON.stringify(SOURCE_VERSION || null)
+      }),
     ],
-  },
-  plugins: [
-    new CopyWebpackPlugin({ patterns: [{ from: '**/*.{html,css,woff,ttf,eot,svg,woff2,ico}', context: 'app/' }] }),
-    new webpack.DefinePlugin({
-      'process.env.SOURCE_VERSION': JSON.stringify(SOURCE_VERSION || null)
-    })
-  ],
-  mode: 'development',
-  devtool: 'source-map',
+    mode: 'development',
+    devtool: 'source-map',
+  };
 };
