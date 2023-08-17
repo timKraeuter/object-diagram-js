f091c74b6a35019aa153fc8f2957151c75de7fc8
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-08-17 10:50:01 +0200

    Move all files one level up

---
+++ starter/debugger/Debugger.js
@@ -0,0 +1,27 @@
+import inherits from 'inherits-browser';
+
+import CoreModule from 'object-diagram-modeler/lib/core';
+import TranslateModule from 'diagram-js/lib/i18n/translate';
+import SelectionModule from 'diagram-js/lib/features/selection';
+import OverlaysModule from 'diagram-js/lib/features/overlays';
+import WebsocketConnectorModule from './websocket';
+
+import BaseViewer from 'object-diagram-modeler/lib/BaseViewer';
+
+export default function Debugger(options) {
+  BaseViewer.call(this, options);
+}
+
+inherits(Debugger, BaseViewer);
+
+// modules the viewer is composed of
+Debugger.prototype._modules = [
+  CoreModule,
+  TranslateModule,
+  SelectionModule,
+  OverlaysModule,
+  WebsocketConnectorModule
+];
+
+// default moddle extensions the viewer is composed of
+Debugger.prototype._moddleExtensions = {};
---
+++ starter/debugger/websocket/index.js
@@ -0,0 +1,6 @@
+import WebsocketConnector from './WebsocketConnector';
+
+export default {
+  __init__: [ 'websocketConnector' ],
+  websocketConnector: [ 'type', WebsocketConnector ]
+};
