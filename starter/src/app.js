268f7f60d0a5bc982ebcee820696cb16255ebddb
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-11-24 09:26:59 +0100

    run prettier on starter

--- starter/src/app.js
+++ starter/src/app.js
@@ -1,170 +1,171 @@
-import "object-diagram-js/assets/odm.css";
-import ODModeler from "object-diagram-js/lib/Modeler";
+import 'object-diagram-js/assets/odm.css';
+import ODModeler from 'object-diagram-js/lib/Modeler';
 
-import emptyBoardXML from "../resources/emptyBoard.xml";
-import sampleBoardXML from "../resources/sampleBoard.xml";
+import emptyBoardXML from '../resources/emptyBoard.xml';
+import sampleBoardXML from '../resources/sampleBoard.xml';
 
 // modeler instance
 const modeler = new ODModeler({
-  container: "#canvas",
-  keyboard: {
-    bindTo: window,
-  },
+    container: '#canvas',
+    keyboard: {
+        bindTo: window,
+    },
 });
 
 /* screen interaction */
 function enterFullscreen(element) {
-  if (element.requestFullscreen) {
-    element.requestFullscreen();
-  } else if (element.mozRequestFullScreen) {
-    element.mozRequestFullScreen();
-  } else if (element.msRequestFullscreen) {
-    element.msRequestFullscreen();
-  } else if (element.webkitRequestFullscreen) {
-    element.webkitRequestFullscreen();
-  }
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+    }
 }
 
 function exitFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.mozCancelFullScreen) {
-    document.mozCancelFullScreen();
-  } else if (document.webkitExitFullscreen) {
-    document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) {
-    document.msExitFullscreen();
-  }
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
 }
 
 const state = {
-  fullScreen: false,
-  keyboardHelp: false,
+    fullScreen: false,
+    keyboardHelp: false,
 };
 document
-  .getElementById("js-toggle-fullscreen")
-  .addEventListener("click", function () {
-    state.fullScreen = !state.fullScreen;
-    if (state.fullScreen) {
-      enterFullscreen(document.documentElement);
-    } else {
-      exitFullscreen();
-    }
-  });
+    .getElementById('js-toggle-fullscreen')
+    .addEventListener('click', function () {
+        state.fullScreen = !state.fullScreen;
+        if (state.fullScreen) {
+            enterFullscreen(document.documentElement);
+        } else {
+            exitFullscreen();
+        }
+    });
 document
-  .getElementById("js-toggle-keyboard-help")
-  .addEventListener("click", function () {
-    state.keyboardHelp = !state.keyboardHelp;
-    let displayProp = "none";
-    if (state.keyboardHelp) {
-      displayProp = "block";
-    }
-    document.getElementById("io-dialog-main").style.display = displayProp;
-  });
+    .getElementById('js-toggle-keyboard-help')
+    .addEventListener('click', function () {
+        state.keyboardHelp = !state.keyboardHelp;
+        let displayProp = 'none';
+        if (state.keyboardHelp) {
+            displayProp = 'block';
+        }
+        document.getElementById('io-dialog-main').style.display = displayProp;
+    });
 document
-  .getElementById("io-dialog-main")
-  .addEventListener("click", function () {
-    state.keyboardHelp = !state.keyboardHelp;
-    let displayProp = "none";
-    if (!state.keyboardHelp) {
-      document.getElementById("io-dialog-main").style.display = displayProp;
-    }
-  });
+    .getElementById('io-dialog-main')
+    .addEventListener('click', function () {
+        state.keyboardHelp = !state.keyboardHelp;
+        let displayProp = 'none';
+        if (!state.keyboardHelp) {
+            document.getElementById('io-dialog-main').style.display =
+                displayProp;
+        }
+    });
 
 /* file functions */
 function openFile(file, callback) {
-  // check file api availability
-  if (!window.FileReader) {
-    return window.alert(
-      "Looks like you use an older browser that does not support drag and drop. " +
-        "Try using a modern browser such as Chrome, Firefox or Internet Explorer > 10.",
-    );
-  }
+    // check file api availability
+    if (!window.FileReader) {
+        return window.alert(
+            'Looks like you use an older browser that does not support drag and drop. ' +
+                'Try using a modern browser such as Chrome, Firefox or Internet Explorer > 10.',
+        );
+    }
 
-  // no file chosen
-  if (!file) {
-    return;
-  }
+    // no file chosen
+    if (!file) {
+        return;
+    }
 
-  const reader = new FileReader();
+    const reader = new FileReader();
 
-  reader.onload = function (e) {
-    const xml = e.target.result;
+    reader.onload = function (e) {
+        const xml = e.target.result;
 
-    callback(xml);
-  };
+        callback(xml);
+    };
 
-  reader.readAsText(file);
+    reader.readAsText(file);
 }
 
-const fileInput = document.createElement("input");
-fileInput.setAttribute("type", "file");
-fileInput.style.display = "none";
+const fileInput = document.createElement('input');
+fileInput.setAttribute('type', 'file');
+fileInput.style.display = 'none';
 document.body.appendChild(fileInput);
-document.addEventListener("change", function (e) {
-  openFile(e.target.files[0], openBoard);
+document.addEventListener('change', function (e) {
+    openFile(e.target.files[0], openBoard);
 });
 
 function openBoard(xml) {
-  // import board
-  modeler.importXML(xml).catch(function (err) {
-    if (err) {
-      return console.error("could not import od board", err);
-    }
-  });
+    // import board
+    modeler.importXML(xml).catch(function (err) {
+        if (err) {
+            return console.error('could not import od board', err);
+        }
+    });
 }
 
 function saveSVG() {
-  return modeler.saveSVG();
+    return modeler.saveSVG();
 }
 
 function saveBoard() {
-  return modeler.saveXML({ format: true });
+    return modeler.saveXML({ format: true });
 }
 
 // bootstrap board functions
-const downloadLink = document.getElementById("js-download-board");
-const downloadSvgLink = document.getElementById("js-download-svg");
+const downloadLink = document.getElementById('js-download-board');
+const downloadSvgLink = document.getElementById('js-download-svg');
 
-const openNew = document.getElementById("js-open-new");
-const openExistingBoard = document.getElementById("js-open-board");
+const openNew = document.getElementById('js-open-new');
+const openExistingBoard = document.getElementById('js-open-board');
 
 function setEncoded(link, name, data) {
-  const encodedData = encodeURIComponent(data);
+    const encodedData = encodeURIComponent(data);
 
-  if (data) {
-    link.classList.add("active");
-    link.setAttribute(
-      "href",
-      "data:application/xml;charset=UTF-8," + encodedData,
-    );
-    link.setAttribute("download", name);
-  } else {
-    link.classList.remove("active");
-  }
+    if (data) {
+        link.classList.add('active');
+        link.setAttribute(
+            'href',
+            'data:application/xml;charset=UTF-8,' + encodedData,
+        );
+        link.setAttribute('download', name);
+    } else {
+        link.classList.remove('active');
+    }
 }
 
 const exportArtifacts = debounce(function () {
-  saveSVG().then(function (result) {
-    setEncoded(downloadSvgLink, "object-diagram.svg", result.svg);
-  });
+    saveSVG().then(function (result) {
+        setEncoded(downloadSvgLink, 'object-diagram.svg', result.svg);
+    });
 
-  saveBoard().then(function (result) {
-    setEncoded(downloadLink, "object-diagram.xml", result.xml);
-  });
+    saveBoard().then(function (result) {
+        setEncoded(downloadLink, 'object-diagram.xml', result.xml);
+    });
 }, 500);
 
-modeler.on("commandStack.changed", exportArtifacts);
-modeler.on("import.done", exportArtifacts);
+modeler.on('commandStack.changed', exportArtifacts);
+modeler.on('import.done', exportArtifacts);
 
-openNew.addEventListener("click", function () {
-  openBoard(emptyBoardXML);
+openNew.addEventListener('click', function () {
+    openBoard(emptyBoardXML);
 });
 
-openExistingBoard.addEventListener("click", function () {
-  // clear input so that previously selected file can be reopened
-  fileInput.value = "";
-  fileInput.click();
+openExistingBoard.addEventListener('click', function () {
+    // clear input so that previously selected file can be reopened
+    fileInput.value = '';
+    fileInput.click();
 });
 
 openBoard(sampleBoardXML);
@@ -172,13 +173,13 @@ openBoard(sampleBoardXML);
 // helpers //////////////////////
 
 function debounce(fn, timeout) {
-  let timer;
+    let timer;
 
-  return function () {
-    if (timer) {
-      clearTimeout(timer);
-    }
+    return function () {
+        if (timer) {
+            clearTimeout(timer);
+        }
 
-    timer = setTimeout(fn, timeout);
-  };
+        timer = setTimeout(fn, timeout);
+    };
 }
--- starter/webpack.config.js
+++ starter/webpack.config.js
@@ -1,60 +1,62 @@
-const webpack = require("webpack");
+const webpack = require('webpack');
 
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const SOURCE_VERSION =
-  process.env.SOURCE_VERSION || process.env.npm_package_gitHead || "dev";
+    process.env.SOURCE_VERSION || process.env.npm_package_gitHead || 'dev';
 
 module.exports = (env) => {
-  let outputPath = __dirname + "/public";
-  let path = "src";
-  let mode = "development";
-  if (env.ghpages) {
-    // GitHub pages expects static files here.
-    outputPath = __dirname + "/../docs";
-    mode = "production";
-  }
-  return {
-    entry: {
-      bundle: [`./${path}/app.js`],
-    },
-    output: {
-      path: outputPath,
-      filename: "app.js",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.xml$/,
-          use: "raw-loader",
+    let outputPath = __dirname + '/public';
+    let path = 'src';
+    let mode = 'development';
+    if (env.ghpages) {
+        // GitHub pages expects static files here.
+        outputPath = __dirname + '/../docs';
+        mode = 'production';
+    }
+    return {
+        entry: {
+            bundle: [`./${path}/app.js`],
         },
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+        output: {
+            path: outputPath,
+            filename: 'app.js',
         },
-        {
-          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-          type: "asset/resource",
-          generator: {
-            filename: "[name][ext]",
-          },
+        module: {
+            rules: [
+                {
+                    test: /\.xml$/,
+                    use: 'raw-loader',
+                },
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: '[name][ext]',
+                    },
+                },
+            ],
         },
-      ],
-    },
-    plugins: [
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: "**/*.{html,css,woff,ttf,eot,svg,woff2,ico}",
-            context: `${path}/`,
-          },
+        plugins: [
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: '**/*.{html,css,woff,ttf,eot,svg,woff2,ico}',
+                        context: `${path}/`,
+                    },
+                ],
+            }),
+            new webpack.DefinePlugin({
+                'process.env.SOURCE_VERSION': JSON.stringify(
+                    SOURCE_VERSION || null,
+                ),
+            }),
         ],
-      }),
-      new webpack.DefinePlugin({
-        "process.env.SOURCE_VERSION": JSON.stringify(SOURCE_VERSION || null),
-      }),
-    ],
-    mode,
-    devtool: "source-map",
-  };
+        mode,
+        devtool: 'source-map',
+    };
 };
